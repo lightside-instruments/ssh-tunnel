5cfc09fb6e8dcc09b6919987c3008f7f4e802fd4
Vladimir Vassilev <vladimir@lightside-instruments.com>
2023-09-27 02:44:20 +0200

    Release ssh-tunnel-1.4

--- ssh-tunnel.sh
+++ ssh-tunnel.sh
@@ -7,7 +7,7 @@ while [ 1 ] ; do
 
 date
 ssh ${REMOTE_USER}@${REMOTE_ADDRESS} -p ${REMOTE_CONNECT_PORT} "pkill -f 'sleep ${MUTEX_ID}'" || true
-ssh -R 0.0.0.0:${REMOTE_LISTEN_PORT}:127.0.0.1:${REMOTE_CONNECT_PORT} ${REMOTE_USER}@${REMOTE_ADDRESS} -p ${REMOTE_CONNECT_PORT} -o ServerAliveInterval=30 -o ServerAliveCountMax=5 -o TCPKeepAlive=yes -i ~/.ssh/id_rsa sleep ${MUTEX_ID}
+ssh -R 0.0.0.0:${REMOTE_LISTEN_PORT}:127.0.0.1:${LOCAL_LISTEN_PORT} ${REMOTE_USER}@${REMOTE_ADDRESS} -p ${REMOTE_CONNECT_PORT} -o ServerAliveInterval=30 -o ServerAliveCountMax=5 -o TCPKeepAlive=yes -i ~/.ssh/id_rsa sleep ${MUTEX_ID}
 echo "$? : Sleep before reconnection ..."
 sleep 60
 
